374f99cab132cd870b00ad4be348b82a99875ab1
qwertypnk <qwertypnk@protonmail.com>
2021-05-23 12:00:05 +0300

    feat: mysql

--- reset.sh
+++ reset.sh
@@ -3,5 +3,6 @@
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 docker rm -f usc
-docker rmi -f ubuntu-server
+docker rmi -f usi
+docker rmi $(docker images -f dangling=true -q)
 $SCRIPTPATH/run.sh
--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-docker build -t ubuntu-server $SCRIPTPATH
-docker run -p 80:80 -p 443:443 -p 8080:8080 -p 8079:8079 -p 8081:8081 -p 3001:3001 --name usc -v /$HOME:/usr/src -dit ubuntu-server
+docker build -t usi $SCRIPTPATH
+docker run -p 80:80 -p 443:443 -p 8080:8080 -p 8079:8079 -p 8081:8081 -p 3001:3001 --name usc -v /$HOME:/usr/src -dit usi
