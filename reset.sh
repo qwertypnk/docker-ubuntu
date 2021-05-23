eaa3e9898a2f663d614968f6d89ff74c86afeb79
qwertypnk <qwertypnk@protonmail.com>
2021-05-23 17:41:56 +0300

    fix: smallfix

--- reset.sh
+++ reset.sh
@@ -4,5 +4,5 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 docker rm -f usc
 docker rmi -f usi
-docker rmi $(docker images -f dangling=true -q)
+docker rmi -f $(docker images -f dangling=true -q)
 $SCRIPTPATH/run.sh
